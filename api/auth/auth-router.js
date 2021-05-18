535586f2c900e25c3c332def244a154f1eea2b87
g3or3 <>
2021-05-18 19:11:23 -0400

    complete

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,42 +1,30 @@
 const router = require("express").Router();
-const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const bcrypt = require("bcryptjs");
+const {
+	checkUsernameExists,
+	validateRoleName,
+	buildToken,
+} = require("./auth-middleware");
+const Users = require("../users/users-model");
 
-router.post("/register", validateRoleName, (req, res, next) => {
-  /**
-    [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
+router.post("/register", validateRoleName, async (req, res) => {
+	let { username, password } = req.body;
 
-    response:
-    status 201
-    {
-      "user"_id: 3,
-      "username": "anna",
-      "role_name": "angel"
-    }
-   */
+	password = await bcrypt.hash(password, 8);
+
+  try {
+    res.status(201).json(await Users.add({ username, password, role_name: req.role_name }));
+  } catch (err) {
+    res.json({message: "Username already exists"})
+  }
 });
 
-
-router.post("/login", checkUsernameExists, (req, res, next) => {
-  /**
-    [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-    response:
-    status 200
-    {
-      "message": "sue is back!",
-      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
-    }
-
-    The token must expire in one day, and must provide the following information
-    in its payload:
-
-    {
-      "subject"  : 1       // the user_id of the authenticated user
-      "username" : "bob"   // the username of the authenticated user
-      "role_name": "admin" // the role of the authenticated user
-    }
-   */
+router.post("/login", checkUsernameExists, async (req, res) => {
+	if (await bcrypt.compare(req.body.password, req.user.password)) {
+		res.json({ message: `${req.user.username} is back!`, token: buildToken(req.user) });
+	} else {
+		res.status(401).json({ message: "Invalid credentials" });
+	}
 });
 
 module.exports = router;
